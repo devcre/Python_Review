9bba4fb344767d82ba51b20f1c1587a87116eb25
devcre <tigertank707@gmail.com>
2019-09-08 20:09:03 +0900

    add comments

--- day03/Combination.py
+++ day03/Combination.py
@@ -8,11 +8,18 @@ def comb(n,r):
 
 # 파스칼 삼각형(Pascal's Triangle)
 def pascal(n,r):
+    # 중첩 리스트 만들기
     table = [[]]*(n-r+1)
+    # 첫 번째 행을 (r+1)개 원소를 모두 1로 초기화
     table[0] = [1]*(r+1)
+
+    # 맨 왼쪽 열을 모두 1로 초기화
     for i in range(1,n-r+1):
         table[i] = [1]
+    
+    # 모든 리스트를 채우는 반복문
     for i in range(1,n-r+1):
+        # 한 행을 채우는 반복문
         for j in range(1,r+1):
             newvalue = table[i][j-1] + table[i-1][j]
             table[i].append(newvalue)
